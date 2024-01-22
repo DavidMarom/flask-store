5ade7eb5fa9c72e6cb4811e5ee55eb7d1728bc47
David Marom <davidmarom.pro@gmail.com>
2024-01-22 15:35:34 +0200

    wip

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 
 app = Flask(__name__)
-
 
 @app.get("/store")
 def get_stores():
@@ -20,10 +20,10 @@ def create_store():
 
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
     
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -31,22 +31,23 @@ def create_item(name):
 
     return item, 201
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
-def get_item(id_item):
+def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
