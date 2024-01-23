aa39200196a32bc9654df39162a38d2c9e8f1808
David Marom <davidmarom.pro@gmail.com>
2024-01-23 09:39:31 +0200

    validations

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from db import stores, items
 
 app = Flask(__name__)
 
+
 @app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}
@@ -13,6 +14,16 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request",
+            )
+        
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -22,9 +33,22 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "name" not in item_data
+        or "store_id" not in item_data
+    ):
+        abort(400, message="Bad request")
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
+
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found")
-    
+
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
